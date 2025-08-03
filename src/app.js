f6457f099b9df27d933e70f4cbcb41bd615b0f30
Vraj Sanghavi <sanghavivraj@gmail.com>
2025-08-03 15:12:00 +0530

    Initial commit for VIDTUBE project

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+// import routers
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import userRouter from "./routes/user.routes.js"
+import { errorHandler } from "./middlewares/error.middlewares.js";
+import videoRoutes from "./routes/video.routes.js";
+
+
+const app = express()
+app.use(
+    cors({
+        origin: process.env.CROS_ORGIN,
+        credentials: true
+    })
+)
+//command middleware
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/videos", videoRoutes)
+
+
+app.use(errorHandler)
+export { app }
